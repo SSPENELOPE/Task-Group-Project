3b040cead8bf6ed7b6ed24b3627369ca0c56b956
Trever <hercztakk@gmail.com>
2022-08-15 21:31:02 -0500

    added div in HTML file, and working on a function in JS to display selected results in cards

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -15,6 +15,7 @@ var searchEventButton = function () {
             } else {
                 return response.json().then(function(data) {
                     console.log(data);
+                    displayResults(data)
                 })
             }
         })
@@ -25,10 +26,37 @@ var searchEventButton = function () {
 };
 
 
+// The below code is a work in progress displaying cards on the homescreen for specified events
 
-var displayResults = function () {
-    //CODE TO DISPLAY RESULTS HERE
-}
+// var displayCards = document.querySelector("#cards")
+
+// var displayResults = function (data) {
+//     //CODE TO DISPLAY RESULTS HERE
+//     var eventData = data._embedded.events
+//     displayCards.append(eventData)
+//     displayCards.innerHTML = eventData
+//     var results = data._embedded.events[i]
+//     for (var i = 0; i < results; i++) {
+//         displayCards.addClass("results")
+//         var eventTitle = displayCards.append("h4")
+//         eventTitle.innerHTML = eventData[i].name
+//         eventTitle.addClass("title");
+//         displayCards.append(eventTitle);
+//         displayCards.append(resultsCard);
+//     }
+//     console.log(eventData[i].name)
+
+// }
+
+// var resultsContainer = $(".results-container");
+//     for (var i = 0; i < data.length; i++) {
+//         var resultsCard = $("<div>");
+//         resultsCard.addClass("results-card")
+//         var eventTitle = $("<h4>");
+//         eventTitle.textContent = data[i]._embedded.events[i].name;
+//         eventTitle.addClass("card-title");
+//         resultsCard.append($eventTitle);
+//         resultsContainer.append($resultsCard);
 
 
 /*               Event Listeners              */
