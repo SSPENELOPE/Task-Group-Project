5a8a95941b8394a234347854a15688dff0fe86b5
SSPENELOPE <typoepping@gmail.com>
2022-08-16 20:51:21 -0400

    passed page = 0 in the .onChange function so when user alters the genre they start at page zero of results, added function for the search bar next to the dropdwon menu

--- assets/JavaScript/concerts.js
+++ assets/JavaScript/concerts.js
@@ -2,8 +2,10 @@
 var resultsContainer = document.getElementById("container-id");
 var page = "0";
 var nextPrev = document.getElementById("next-prev");
-var musicPrev =  document.getElementById("music-prev");
+var musicPrev = document.getElementById("music-prev");
 var musicNext = document.getElementById("music-next");
+var userSearch = document.getElementById("search-btn");
+var userInput = document.getElementById("music-input");
 
 // Results Card Variables
 var concertDateTime = document.querySelectorAll(".date-time");
@@ -17,13 +19,13 @@ var musicCardResults = document.querySelectorAll(".results-card");
 var searchByGenre = function () {
     var selectedValue = $("#small").val();
     if (selectedValue == "Rap/Hiphop") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&size=6&page="+page+"&genreId=KnvZfZ7vAv1&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&size=6&page=" + page + "&totalPages&genreId=KnvZfZ7vAv1&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     } else if (selectedValue == "Alternative") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page="+page+"&genreId=KnvZfZ7vAvv&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&totalPages&genreId=KnvZfZ7vAvv&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
     } else if (selectedValue == "Country") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page="+page+"&genreId=KnvZfZ7vAv6&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&totalPages&genreId=KnvZfZ7vAv6&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
     } else {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page="+page+"&genreId=KnvZfZ7vAeA&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&totalPages&genreId=KnvZfZ7vAeA&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     };
 
     fetch(apiUrl, {
@@ -36,7 +38,7 @@ var searchByGenre = function () {
             } else {
                 return response.json().then(function (data) {
                     // Console loging the returned data so we can see what we need to get
-                    /*  console.log(data); */
+                    console.log(data)
                     displayGenreResults(data);
                 });
             }
@@ -46,21 +48,24 @@ var searchByGenre = function () {
         });
 };
 
-/* var searchForArtistOrEvent = function () {
+function searchByInput(Event) {
+    Event.preventDefault();
     var selectedValue = $("#small").val();
-     var userInput = $("#search-btn");
-    if (selectedValue == "Rap/Hiphop") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&genreId=KnvZfZ7vAv1&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
-    } else if (selectedValue == "Alternative") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&genreId=KnvZfZ7vAvv&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
-    } else if (selectedValue == "Country") {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&genreId=KnvZfZ7vAv6&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
+    var musicInput = userInput.value /* $("#music-input").val(); */
+    if (selectedValue == "Rap/Hiphop" && musicInput) {
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&size=6&page=" + page + "&genreId=KnvZfZ7vAv1&keyword=" + musicInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+    } else if (selectedValue == "Alternative" && musicInput) {
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&genreId=KnvZfZ7vAvv&keyword=" + musicInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
+    } else if (selectedValue == "Country" && musicInput) {
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&genreId=KnvZfZ7vAv6&keyword" + musicInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq"
     } else {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?&genreId=KnvZfZ7vAeA&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=6&page=" + page + "&genreId=KnvZfZ7vAeA&keyword=" + musicInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     };
 
+
+
     fetch(apiUrl, {
-        method: "get",
+        method: "POST",
     }
     )
         .then(function (response) {
@@ -69,17 +74,14 @@ var searchByGenre = function () {
             } else {
                 return response.json().then(function (data) {
                     // Console loging the returned data so we can see what we need to get
-                    console.log(data);
                     displayGenreResults(data);
                 });
             }
         })
-
         .catch(function (error) {
             alert(error);
         });
-} */
-
+}
 
 function displayGenreResults(data) {
     var events = data._embedded.events;
@@ -87,36 +89,43 @@ function displayGenreResults(data) {
 
     for (var i = 0; i < events.length; i++) {
         var concertDate = events[i].dates.start.dateTime;
-        var concertReadableDate = new Date(concertDate); 
+        var concertReadableDate = new Date(concertDate);
         concertDateTime[i].textContent = concertReadableDate.toDateString();
         concertTitle[i].textContent = events[i].name;
         concertLink[i].textContent = "Purchase Tickets Now";
         concertLink[i].href = events[i].url;
-        venue[i].textContent = events[i]._embedded.venues[0].city.name;
-        musicCardResults[i].setAttribute("style", "background: url("+events[i].images[1].url+")");
+        venue[i].textContent = events[i]._embedded.venues[0].city.name + "," + " " + events[i]._embedded.venues[0].state.name;
+        musicCardResults[i].setAttribute("style", "background: url(" + events[i].images[1].url + ")");
     }
-   
+
 };
 
- var nextPage = function () {
-     if (page >= 0) {
+var nextPage = function () {
+    if (page >= 0) {
         page++;
         console.log(page);
         searchByGenre();
-     }   
+    }
 }
 
 var prevPage = function () {
     if (page >= 1) {
         page--;
         searchByGenre();
-     } else {
+    } else {
         return;
-     }  
+    }
 }
 
 // Event Listener for genre select menu
-$("#small").on("change", searchByGenre);
+$("#small").on("change", function() {
+    searchByGenre(page = "0");
+});
+
+userSearch.addEventListener("click", function (Event) {
+    Event.preventDefault();
+    searchByInput();
+});
 
 musicNext.addEventListener("click", nextPage);
 
