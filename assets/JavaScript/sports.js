ee787665f8aa07cdfd774872266ba2830a29e500
sara remde <sara@remde.us>
2022-08-21 23:17:21 -0500

    assets/JavaScript/sports.js

--- assets/JavaScript/sports.js
+++ assets/JavaScript/sports.js
@@ -5,14 +5,23 @@ var userSearch = document.getElementById("sportSearch-btn");
 var userInput = document.getElementById("music-input");
 var sportsCards = document.getElementById("container-id");
 
-// the result car variables
+// the result card variables
 var sportsResutlsWrapper = document.querySelector(".sports-results");
 var sportsDateTime = document.querySelector(".date-time");
 var venue = document.querySelectorAll (".venue-text");
 var sportsTitle = document.querySelectorAll (".card-title");
 var sportsLink = document.querySelectorAll(".website-link");
 var sportsCardsResults = document.querySelectorAll(".results-card");
+var sportsImg = document.querySelectorAll(".sports-img")
 
+// brewery card variables
+var beweryInput = document.getElementById("brewery-input");
+var brewerySearch = document.getElementById("brewery-search");
+var breweryName = document.querySelectorAll(".brewery-name");
+var breweryAddress = document.querySelectorAll(".brewery-address");
+var breweryUrl = document.querySelectorAll(".brewery-url");
+var breweryResults = document.querySelectorAll(".brewery-results");
+var brewPage = "1";
 
 
 // functions
@@ -20,7 +29,7 @@ var sportsCardsResults = document.querySelectorAll(".results-card");
 var searchBySport = function () {
     var selectedValue = $("#medium").val();
     if (selectedValue ) {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword="+selectedValue+"&page=" + page + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=8&keyword="+selectedValue+"&page=" + page + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     } 
 //keyword="+selectedValue+"
 
@@ -44,16 +53,21 @@ var searchBySport = function () {
 //display sports results funciton 
 function displaySportsResults(data) {
     var events = data._embedded.events;
+    
     console.log(events);
+    
     if (sportsCards.style.display == "none") {
         sportsCards.style.display = "flex" ;
     }
     for (var i= 0; i< events.length; i++ ) {
+    
         venue[i].textContent = events[i]._embedded.venues[0].name;
         sportsTitle[i].textContent = events[i].name;
         sportsLink[i].textContent = "Purchase Tickets Now";
-        sportsLink[i].textContent = events[i].url;
-
+        sportsLink[i].href = events[i].url;
+        /* sportsImg[i].textContent = events[i].images[0].attribution; */
+           
+        sportsImg[i].setAttribute("style", "background: url("+ events[i].images[1].url + ");");
         
     }
 
@@ -65,10 +79,10 @@ function displaySportsResults(data) {
 // search by sport and input function
 function searchByInputAndSport(e) {
     e.preveentDefault();
-    var selectedValue = $("small").val();
-    var SportsInput = userInput.value; 
+    var selectedValue = $("medium").val();
+    var sportsInput = userInput.value; 
     if (selectedValue == "Football" && sportsInput) {
-        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=8&page=" + page + "&genreId=KnvZfZ7vAv1&keyword=" + sportsInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
+        var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&page=" + page + "&genreId=KnvZfZ7vAv1&keyword=" + sportsInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     } else if (selectedValue == "Baseball" && sportsInput) {
         var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=8&page=" + page + "&genreId=KnvZfZ7vAv1&keyword=" + sportsInput + "&apikey=taiF3boXdKk17IQ69YlGzA1O29aTWlnq";
     } else if (selectedValue == "Basketball" && sportsInput) {
@@ -93,6 +107,52 @@ function searchByInputAndSport(e) {
         alert("no evnets found");
     });
 }
+
+// brewery API function 
+var getbrewery = function () {
+    var breweryCity = breweryInput.value;
+    var selectedState = breweryState.value;
+
+    if (selectedState == true && breweryCity) {
+        var breweryAPI = "https://api.openbrewerydb.org/breweries?by_city="+breweryCity+"&by_state"+selectedState+"&per_page=3&page="+brewPage;
+    } else {
+        var breweryApi = "https://api.openbrewerydb.org/breweries?by_city="+breweryCity+"&per_page=3&page="+brewPage;
+    };
+
+    fetch(breweryApi, {
+        method: "GET",
+    })
+    .then(function (response) {
+        if(!response.ok) {
+            alert(response.statusText)
+        } else {
+            return response.json().then(function (brewData) {
+                console.log(brewData)
+                if (brewData.length == 0) {
+                    swal ("sorry we cannot find brewerys in that area");
+                    return;
+                } else {
+                    displayBreweryResults(brewData);
+                };
+            })
+        }
+    });
+};
+
+// function to bdispkay brewery results
+function displayBreweryResluts(brewData) {
+    if (breweryResults.style.display == "none") {
+        breweryResults.style.display = "flex";
+    };
+
+    for (var i = 0; i < brewData.length; i ++) {
+        breweryName[i].textContent = brewData[i].name;
+        breweryAddress[i].textContent = brewData[i].street;
+        brewyAddress[i].href = "https://maps.google.com/?" + brewData[i].stret;
+        breweryUrl[i].href = brewData[i].href = brewData[i].website_url;
+    };
+}
+
 
 //next page 
 var nextPage = function (event) {
